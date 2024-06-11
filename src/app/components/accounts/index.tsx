22f219e898bfa1cd245a5bf2389b5b0a1134963f
lesedi-k <97709988+lesedi-k@users.noreply.github.com>
2024-06-11 12:35:04 -0400

    view hom ein progress

--- src/app/components/accounts/index.tsx
+++ src/app/components/accounts/index.tsx
@@ -1,6 +1,6 @@
 export default  function Accounts({ title, acc }: { title: string; acc:any[] }) {
     return (
-        <div className=" flex flex-col h-1/2 w-full rounded-xl mb-4 p-4 pb-10 max-h-screen whiteCard">
+        <div className=" flex flex-col h-1/2 w-full rounded-xl mb-4 p-4 pb-10 max-h-screen card">
             <div className="flex flex-row justify-between">
                 <h2 className="mb-1">{title}</h2> 
                 <div className="hover:cursor-pointer w-1/12">
@@ -10,8 +10,8 @@ export default  function Accounts({ title, acc }: { title: string; acc:any[] }) 
             
             {acc.map((a, index)=> (
                 <div key={a + "" + index} className="flex flex-row justify-between p-2 mb-2
-                        rounded-lg bg-slate-200/30
-                        hover:bg-slate-200/50 hover:rounded-lg hover:cursor-pointer
+                        rounded-lg bg-slate-50/60
+                        hover:bg-slate-50 hover:rounded-lg hover:cursor-pointer
                 ">
                     {/* TODO: Insert card icon */}
                     <h4>{a.name}</h4>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,8 +22,8 @@ export default function Home() {
             <HeaderBar/>
 
             {/* Pages */}
-            {/* <Landing/> */}
-            <DashBoard/>
+            <Landing/>
+            {/* <DashBoard/> */}
           </div>
         </div>
         
